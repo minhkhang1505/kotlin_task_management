bffe039d72c792fb14a37f2012db65d8b6aa0115
Nguyễn Minh Khang <mkhang040506@gmail.com>
2025-06-16 00:34:57 +0700

    handle some button and textfield on Repeat Page

--- app/src/main/java/com/nguyenminhkhang/taskmanagement/ui/floataction/RepeatPage.kt
+++ app/src/main/java/com/nguyenminhkhang/taskmanagement/ui/floataction/RepeatPage.kt
@@ -10,15 +10,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,6 +34,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -43,8 +50,10 @@ fun RepeatPage(navController: NavController) {
     val dayItems = listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
     var timeAndDay by remember { mutableStateOf("") }
     var isSelectedDay by remember { mutableStateOf(false) }
-    var isShowDatePicker by remember { mutableStateOf(false) }
+    var isShowStartDatePicker by remember { mutableStateOf(false) }
+    var isShowEndDatePicker by remember { mutableStateOf(false) }
     var selectedStartDay by remember { mutableStateOf("DD/MM/YYYY") }
+    var selectedEndDay by remember { mutableStateOf("DD/MM/YYYY") }
     var selectedDays by remember { mutableStateOf("") }
     var isShowTimePicker by remember { mutableStateOf(false) }
     var selectedTime by remember { mutableStateOf("Set time") }
@@ -54,6 +63,16 @@ fun RepeatPage(navController: NavController) {
     var selectedRepeatType by remember { mutableStateOf(repeatType[0]) }
     val radioOptions = listOf("Never", "At", "After")
     val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
+
+    val OUTLINETEXTFIELD_COLOR = OutlinedTextFieldDefaults.colors(
+        disabledTextColor = MaterialTheme.colorScheme.onSurface,
+        disabledContainerColor = Color.Transparent,
+        disabledBorderColor = MaterialTheme.colorScheme.outline,
+        disabledLeadingIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledTrailingIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledPlaceholderColor = MaterialTheme.colorScheme.onSurfaceVariant,
+    )
 
     Scaffold(
         topBar = {
@@ -92,15 +111,30 @@ fun RepeatPage(navController: NavController) {
                     onValueChange = { repeatTimes = it },
                     keyboardOptions = KeyboardOptions(keyboardType =  KeyboardType.Number),
                     modifier = Modifier.weight(0.2f).padding(end = 8.dp),
+                    maxLines = 1,
+                    singleLine = true,
                 )
-                OutlinedTextField(
-                    value = "",
-                    placeholder = { Text(selectedRepeatType) },
-                    onValueChange = {
-                        isShowDropdownSelectedType = true
-                        selectedRepeatType = it },
-                    modifier = Modifier.padding(end = 8.dp).weight(0.8f),
-                )
+                Box (
+                    modifier = Modifier.padding(end = 8.dp).weight(0.8f).clickable {  }
+                ) {
+                    OutlinedTextField(
+                        value = "",
+                        placeholder = { Text(selectedRepeatType) },
+                        onValueChange = {
+                            isShowDropdownSelectedType = true
+                            selectedRepeatType = it },
+                        trailingIcon = {
+                            Icon(Icons.Default.KeyboardArrowDown, contentDescription = "Dropdown",
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .clickable { isShowDropdownSelectedType = !isShowDropdownSelectedType }
+                            )
+                        },
+                        readOnly = true,
+                        enabled = false,
+                        colors = OUTLINETEXTFIELD_COLOR,
+                    )
+                }
             }
             Row(
                 modifier = Modifier.fillMaxWidth().padding(8.dp),
@@ -116,33 +150,40 @@ fun RepeatPage(navController: NavController) {
             }
 
             // Time Selection
-            OutlinedTextField(
-                value = selectedTime,
-                onValueChange = {},
-                modifier = Modifier
-                    .padding(end = 8.dp)
-                    .fillMaxWidth()
-                    .clickable { isShowTimePicker = true },
-                trailingIcon = {
-                    Icon(Icons.Default.Clear, contentDescription = "Clear",
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .clickable { selectedTime = "Set time" }
-                    )
-                },
-                readOnly = true,
-            )
+            Box(modifier = Modifier.clickable { isShowTimePicker = true }) {
+                OutlinedTextField(
+                    value = selectedTime,
+                    onValueChange = {},
+                    modifier = Modifier
+                        .padding(end = 8.dp)
+                        .fillMaxWidth(),
+                    trailingIcon = {
+                        Icon(Icons.Default.Clear, contentDescription = "Clear",
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .clickable { selectedTime = "Set time" }
+                        )
+                    },
+                    readOnly = true,
+                    enabled = false,
+                    colors = OUTLINETEXTFIELD_COLOR
+                )
+            }
+
             // Date Selection
             Text(text = "Start Date", modifier = Modifier.padding(top = 8.dp, bottom = 4.dp))
-            OutlinedTextField(
-                value = selectedStartDay,
-                onValueChange = {},
-                modifier = Modifier
-                    .padding(end = 8.dp)
-                    .fillMaxWidth()
-                    .clickable { isShowDatePicker = true },
-                readOnly = true,
-            )
+            Box(modifier = Modifier.clickable { isShowStartDatePicker = true }) {
+                OutlinedTextField(
+                    value = selectedStartDay,
+                    onValueChange = {},
+                    modifier = Modifier
+                        .padding(end = 8.dp)
+                        .fillMaxWidth(),
+                    readOnly = true,
+                    enabled = false,
+                    colors = OUTLINETEXTFIELD_COLOR
+                )
+            }
             // Radio Buttons for End Condition
             Text(text = "End Condition", modifier = Modifier.padding(top = 8.dp, bottom = 4.dp))
             Column(
@@ -174,14 +215,24 @@ fun RepeatPage(navController: NavController) {
                                     modifier = Modifier.padding(end = 8.dp),
                                     style = MaterialTheme.typography.bodyLarge
                                 )
-                                OutlinedTextField(
-                                    value = selectedStartDay,
-                                    onValueChange = { selectedStartDay = it },
-                                    modifier = Modifier
-                                        .weight(1f)
-                                        .padding(end = 8.dp),
-                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-                                )
+                                Box(modifier = Modifier.weight(1f).padding(end = 8.dp)
+                                    .selectable(
+                                        selected = ("At" == selectedOption),
+                                        onClick = {
+                                            onOptionSelected("At")
+                                            isShowEndDatePicker = true
+                                        },
+                                        role = Role.RadioButton
+                                    )
+                                ) {
+                                    OutlinedTextField(
+                                        value = selectedEndDay,
+                                        onValueChange = {},
+                                        readOnly = true,
+                                        enabled = false,
+                                        colors = OUTLINETEXTFIELD_COLOR
+                                    )
+                                }
                             }
                             "After" -> {
                                 Text(
@@ -194,8 +245,20 @@ fun RepeatPage(navController: NavController) {
                                     onValueChange = { repeatTimes = it },
                                     modifier = Modifier
                                         .weight(0.2f)
-                                        .padding(end = 8.dp),
-                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                                        .padding(end = 8.dp)
+                                        .selectable(
+                                            selected = ("After" == selectedOption),
+                                            onClick = { onOptionSelected("After") },
+                                            role = Role.RadioButton
+                                        ),
+                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                                    enabled = ("After" == selectedOption),
+                                    colors = OUTLINETEXTFIELD_COLOR,
+                                    maxLines = 1,
+                                    singleLine = true,
+                                    keyboardActions = KeyboardActions(
+                                        onDone = { }
+                                    )
                                 )
                                 Text(
                                     text = "occurrence",
@@ -209,12 +272,14 @@ fun RepeatPage(navController: NavController) {
                     }
                 }
             }
-            if(isShowDatePicker) {
+            if(isShowStartDatePicker || isShowEndDatePicker) {
                 DatePickerModal(
-                    onDismiss = { isShowDatePicker = false },
+                    onDismiss = { isShowStartDatePicker = false
+                                  isShowEndDatePicker = false },
                     onDateSelected = { date ->
-                        selectedStartDay = date
-                        isShowDatePicker = false
+                        if(isShowStartDatePicker) selectedStartDay = date else selectedEndDay = date
+                        isShowStartDatePicker = false
+                        isShowEndDatePicker = false
                     },
                 )
             }
@@ -227,6 +292,7 @@ fun RepeatPage(navController: NavController) {
                     },
                 )
             }
+            if(isShowDropdownSelectedType){}
         }
     }
 }
