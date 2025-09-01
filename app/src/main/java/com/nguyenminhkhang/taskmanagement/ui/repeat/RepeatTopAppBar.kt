b35caa81facd1df69f0d7dee48735ab395d677c1
Nguyễn Minh Khang <mkhang040506@gmail.com>
2025-09-01 21:56:20 +0700

    fix top bar at repeat page and task detail page

--- app/src/main/java/com/nguyenminhkhang/taskmanagement/ui/repeat/RepeatTopAppBar.kt
+++ app/src/main/java/com/nguyenminhkhang/taskmanagement/ui/repeat/RepeatTopAppBar.kt
@@ -1,43 +1,53 @@
 package com.nguyenminhkhang.taskmanagement.ui.repeat
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RepeatTopAppBar(
     onSave: () -> Unit,
     onNavigationBack: () -> Unit,
 ) {
-    TopAppBar(
-        modifier = Modifier.padding(start = 8.dp, end = 12.dp),
-        title = { Text("Repeat") },
-        navigationIcon = {
+    Row(
+        modifier = Modifier.fillMaxWidth().size(56.dp)
+            .padding(horizontal = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Icon(
                 Icons.Default.Clear, contentDescription = "Back",
                 modifier = Modifier
                     .padding(8.dp)
                     .clickable { onNavigationBack() }
             )
-        },
-        actions = {
-            Text("Save",
-                modifier = Modifier
-                    .padding(8.dp)
-                    .clickable {
-                        onSave()
-                        onNavigationBack()
-                    }
+            Text(
+                "Repeat",
+                fontSize = MaterialTheme.typography.titleLarge.fontSize,
             )
         }
-    )
+        Text("Save",
+            modifier = Modifier
+                .padding(8.dp)
+                .clickable {
+                    onSave()
+                    onNavigationBack()
+                }
+        )
+    }
 }
--- app/src/main/java/com/nguyenminhkhang/taskmanagement/ui/taskdetail/TaskDetailTopBar.kt
+++ app/src/main/java/com/nguyenminhkhang/taskmanagement/ui/taskdetail/TaskDetailTopBar.kt
@@ -1,57 +1,58 @@
 package com.nguyenminhkhang.taskmanagement.ui.taskdetail
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.nguyenminhkhang.taskmanagement.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskDetailTopAppBar (
     isFavorite: Boolean,
     onFavoriteClick: () -> Unit,
     onNavigateBack: () -> Unit
 ) {
-    TopAppBar(
-        title = { Text(text = "", style = MaterialTheme.typography.titleLarge) },
-        actions = {
-            IconButton(
-                onClick = { onFavoriteClick() },
-                content = {
-                    Icon(
-                        painter = painterResource(
-                            id = if (isFavorite) {
-                                R.drawable.baseline_star_24
-                            } else {
-                                R.drawable.baseline_star_outline_24
-                            },
-                        ),
-                        contentDescription = "Favorite",
-                        tint = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f)
-                    )
-                }
-            )
-        },
-        navigationIcon = {
-            Icon(
-                painter = painterResource(R.drawable.baseline_arrow_back_ios_24),
-                contentDescription = "Back",
-                modifier = Modifier
-                    .padding(8.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .clickable { onNavigateBack() },
-            )
-        }
-    )
+    Row(
+        modifier = Modifier.fillMaxWidth().size(56.dp)
+            .padding(horizontal = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.baseline_arrow_back_ios_24),
+            contentDescription = "Back",
+            modifier = Modifier
+                .padding(8.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .clickable { onNavigateBack() },
+        )
+        IconButton(
+            onClick = { onFavoriteClick() },
+            content = {
+                Icon(
+                    painter = painterResource(
+                        id = if (isFavorite) {
+                            R.drawable.baseline_star_24
+                        } else {
+                            R.drawable.baseline_star_outline_24
+                        },
+                    ),
+                    contentDescription = "Favorite",
+                    tint = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f)
+                )
+            }
+        )
+    }
 }
